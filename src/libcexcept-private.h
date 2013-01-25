d76d566ca88fe54630f34be769386477c39ab118
Pedro Alves <palves@redhat.com>
2013-01-25 15:36:52 +0000

    Garbage collection in libcexcept-private.h.

--- src/libcexcept-private.h
+++ src/libcexcept-private.h
@@ -18,39 +18,8 @@
 #ifndef _LIBCEXCEPT_PRIVATE_H_
 #define _LIBCEXCEPT_PRIVATE_H_
 
-#include <stdbool.h>
-#include <syslog.h>
-
 #include <cexcept/libcexcept.h>
 
-static inline void __attribute__((always_inline, format(printf, 2, 3)))
-cexcept_log_null(struct cexcept_ctx *ctx, const char *format, ...) {}
-
-#define cexcept_log_cond(ctx, prio, arg...) \
-  do { \
-    if (cexcept_get_log_priority(ctx) >= prio) \
-      cexcept_log(ctx, prio, __FILE__, __LINE__, __FUNCTION__, ## arg); \
-  } while (0)
-
-#ifdef ENABLE_LOGGING
-#  ifdef ENABLE_DEBUG
-#    define dbg(ctx, arg...) cexcept_log_cond(ctx, LOG_DEBUG, ## arg)
-#  else
-#    define dbg(ctx, arg...) cexcept_log_null(ctx, ## arg)
-#  endif
-#  define info(ctx, arg...) cexcept_log_cond(ctx, LOG_INFO, ## arg)
-#  define err(ctx, arg...) cexcept_log_cond(ctx, LOG_ERR, ## arg)
-#else
-#  define dbg(ctx, arg...) cexcept_log_null(ctx, ## arg)
-#  define info(ctx, arg...) cexcept_log_null(ctx, ## arg)
-#  define err(ctx, arg...) cexcept_log_null(ctx, ## arg)
-#endif
-
 #define CEXCEPT_EXPORT __attribute__ ((visibility("default")))
-
-void cexcept_log(struct cexcept_ctx *ctx,
-           int priority, const char *file, int line, const char *fn,
-           const char *format, ...)
-           __attribute__((format(printf, 6, 7)));
 
 #endif
